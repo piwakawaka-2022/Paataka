5378a732a74fcf343a8d9f9b187af258358bb767
Tim Liew <74855211+Tweemo@users.noreply.github.com>
2022-05-05 15:12:00 +1200

    completed navbar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useNavigate } from 'react-router-dom'
-
 import { logoutUser } from '../actions/auth'
 
 function Nav () {
@@ -9,11 +8,12 @@ function Nav () {
   const dispatch = useDispatch()
   const auth = useSelector(redux => redux.auth)
 
-  const [burgerVisible, setBurgerVisible] = useState(false)
+  // const [burgerVisible, setBurgerVisible] = useState(false)
 
-  const toggleBurger = () => {
-    setBurgerVisible(currentBurgerState => !currentBurgerState)
-  }
+  // const toggleBurger = () => {
+  //   setBurgerVisible(currentBurgerState => !currentBurgerState)
+  // }
+
   const logout = () => {
     const confirmSuccess = () => navigateTo('/')
     dispatch(logoutUser(confirmSuccess))
@@ -21,27 +21,23 @@ function Nav () {
 
   return <nav className="navbar">
     <div className="container">
-      <div className="navbar-brand">
-        <span onClick={toggleBurger} className={`navbar-burger burger ${burgerVisible ? 'is-active' : ''}`} data-target="navbarMenuHeroA">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-      </div>
-      <div id="navbarMenuHeroA" className={`navbar-menu ${burgerVisible ? 'is-active' : ''}`}>
-        <div className="navbar-end">
-          { auth.isAuthenticated
-            ? (
-              <Link to='/' className="navbar-item is-large" onClick={() => logout()}>Logout</Link>
-            )
-            : (
-              <>
-                <Link onClick={toggleBurger} className="navbar-item is-large" to='/login'>Login</Link>
-                <Link onClick={toggleBurger} className="navbar-item" to='/register'>Register</Link>
-              </>
-            )
-          }
-        </div>
+      <div className="navbar-end">
+        {auth.isAuthenticated ? (
+          <div className="nav-container">
+            <div className="pataka">Pātaka</div>
+            <Link to='/' onClick={() => logout()}>Logout</Link>
+            <Link className="clickies" to='listings'>Listings</Link>
+            <Link className="clickies" to='addListing'>Add Food</Link>
+          </div>
+        )
+          : (
+            <div className="nav-container">
+              <div className="pataka" >Pātaka</div>
+              <Link className="clickies" to='login'>Login</Link>
+              <Link className="clickies" to='listings'>Listings</Link>
+            </div>
+          )
+        }
       </div>
     </div>
   </nav>
