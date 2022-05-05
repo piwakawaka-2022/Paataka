2924c3201efe1e6aa168553076602b61249ed821
Ngahine Wakefield <28144736+ngas-wakefield@users.noreply.github.com>
2022-05-05 16:22:44 +1200

    edits

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -8,23 +8,35 @@ function LandingPage () {
     <h1>Nau mai, haere mai ki a Pātaka!</h1>
     <div className='home-container'></div>
     <div className='home-values'>
-        <p>Our values go here</p>
+        <p>Main home value greeting
+</p>
     </div>
     <div className='home-facts'>
-        <div className='fact1'>Fact 1</div>
-        <div className='fact2'>Fact 2</div>
-        <div className='fact3'>Fact 3</div>
+        <div className='fact1'>There is enough food produced in the world to feed every living human.</div>
+        <div className='fact2'>New Zealand households throw away 157,389 tonnes of food a year. </div>
+        <div className='fact3'>We throw away 29 million loaves of bread a year! Nearly one-third of the food we waste is vegetables.</div>
     </div>
     
-    <Link to="/AddListings"><button>
+    <Link to="/addlistings"><button>
               Got food?
             </button>
             </Link>
 
-            <Link to="/Listings"><button>
+            <Link to="/listings"><button>
               Need food?
             </button>
             </Link>
+
+
+
+            <div className='home-values'>
+        <p>Food always tastes better when you share it with someone - Josh
+</p>
+<p>Connecting communities (whanaungitanga) through tackling food inequality
+Reducing waste/environmental impact of wasted food (Kaitiakitanga) - Luke
+</p>
+    </div>
+
         </div>
       )
 }
