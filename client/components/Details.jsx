b81c828ba48a1528f489a2d946478e16522bbac7
Josh Liuaana <98368294+josh-liuaana@users.noreply.github.com>
2022-05-09 16:21:37 +1200

    details listings pages css update

--- client/components/Details.jsx
+++ client/components/Details.jsx
@@ -21,7 +21,10 @@ function Details () {
     <div className='details-main'>
       <div className='go-back'>
         {/* redirect back to listings */}
-        <Link to="/listings"><button>Back to Listings</button></Link>
+        <Link to="/listings">
+          <button className='go-back-button'>Back to Listings</button>
+        </Link>
+        
       </div>
       <div className='details-container'>
         <div>
@@ -31,8 +34,8 @@ function Details () {
           {/* <img>user info</img> */}
           <h1 className='details-title'> {food?.title} </h1>
           <p className='details-description'> {food?.description} </p>
-          <p className='details-expiry'> {food?.expiry_date} </p>
-          <p className='details-phone'> {food?.phone} </p>
+          <p className='details-expiry'> <em>Expiry:</em> {food?.expiry_date} </p>
+          <p className='details-phone'> <em>{food?.username} :</em> {food?.phone} </p>
           {/* <Comments /> */}
         </div>
       </div>
