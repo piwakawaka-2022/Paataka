57cc4e365faaaae28c0a85a7c65ad93d52041d5c
Grace James <gracejames@Graces-MacBook-Air.local>
2022-05-11 13:56:24 +1200

    for merge

--- client/components/Comment.jsx
+++ client/components/Comment.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en.json'
 function Comment (props) {
+  TimeAgo.addLocale(en)
+  const timeAgo = new TimeAgo('en-US')
+ console.log(props.dateCreated)
+  const date = new Date(props.dateCreated)
+  console.log(new Date())
+  console.log(date)
+  const timeSince = timeAgo.format(date)
   return (
     <div className='bubble'>
       <div className='bubble-content'>
-        <div className='bubble-title'>{props.name} • {props.dateCreated}</div>
+        <div className='bubble-title'>
+          {props.name} • {timeSince}
+        </div>
         <p>{props.comment}</p>
       </div>
     </div>
