190e2ea4b60bde5288d8bd270dddd8d19750c95f
Ngahine Wakefield <28144736+ngas-wakefield@users.noreply.github.com>
2022-06-01 13:04:42 +1200

    setting up maps api

---
+++ client/apis/maps.js
@@ -0,0 +1,1 @@
+const key = process.env.MAPS_KEY
---
+++ client/components/Map.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState, useRef } from 'react'
+
+const Map = () => {
+  // const ref = React.useRef(null);
+  // const [map, setMap] = React.useState();
+
+  // React.useEffect(() => {
+  //   if (ref.current && !map) {
+  //     setMap(new window.google.maps.Map(ref.current, {}));
+  //   }
+  // }, [ref, map]);
+
+  return (<div><p> map</p> </div>)
+};
+
+export default Map
